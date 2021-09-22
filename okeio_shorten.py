39cf2836b6b1a9633d297de45a8628f69f448d67
joyal0 <76865773+joyal0@users.noreply.github.com>
2021-09-22 20:01:28 +0530

    Update okeio_shorten.py

--- okeio_shorten.py
+++ okeio_shorten.py
@@ -3,7 +3,7 @@ import requests
 access_token = 'your_oke.io_access_token'
 
 # Shorten long URL
-custom_alias = input("Enter the custom alias you would like for the shortened url \n")      #the returned shortened url will be of the form https://www.oke.io/{custom_alias}
+custom_alias = input("Enter the custom alias you would like for the shortened url \n")      #the returned shortened url will be of the form https://www.oke.io/{custom_alias} if alias available
 long_url = "your_website_address_that_need_to_be_shortened"
 api_url = f"https://oke.io/api/?api={access_token}&url={long_url}&alias={custom_alias}"
 shortened_url = requests.get(api_url).json()
